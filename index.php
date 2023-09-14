3c9cf046e03380a07e43293925e01046d5231e11
Cloud <ruben.as532@gmail.com>
2023-09-14 10:02:16 +0200

    Initial loop

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-
+include('class/Move.php');
+$move = Move::DOWN;
+while($move != 0) {
+	echo '1-DOWN'.PHP_EOL;
+	echo '2-LEFT'.PHP_EOL;
+	echo '3-RIGHT'.PHP_EOL;
+	echo '4-ROTATE'.PHP_EOL;
+	echo '0-END OF GAME'.PHP_EOL;
+	$move = readline("Next move!");
+}
 
 
 ?>
